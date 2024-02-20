a5908f7d0a1e3028e2e71d5d0fb52169eeb801df
Zachary Kirtz <zach.kirtz@gmail.com>
2024-02-20 17:41:56 -0600

    console: console_read_strn: support backspace (fixes #23)

--- console_win.c
+++ console_win.c
@@ -1,8 +1,11 @@
 #include "console_win.h"
 #include <assert.h>
+#include <curses.h>
 #include <limits.h>
 #include <ncurses.h>
 #include <pthread.h>
+
+#include <stdlib.h>
 
 static bool g_initialized = false;
 
@@ -20,6 +23,37 @@ static bool _get_and_clear_intrpt(void) {
 	return out;
 }
 
+static int _get_char_impl(void) {
+	int out;
+	do {
+		out = wgetch(g_window);
+		if(_get_and_clear_intrpt()) {
+			return ERR;
+		}
+	} while(out == ERR);
+
+	return out;
+}
+
+void _insert_char_into_str(char* str, int pos, int len) {
+}
+
+void _shift_str_backward(char* str, int start_pos, int len, int x, int y) {
+	wmove(g_window, y, x + start_pos);
+
+	int pos = start_pos;
+	while(pos < len - 1 && str[pos] != 0) {
+		str[pos] = str[pos + 1];
+		if(str[pos] != 0) {
+			waddch(g_window, str[pos]);
+		}
+		pos++;
+	}
+	waddch(g_window, ' ');
+
+	wmove(g_window, y, x + start_pos);
+}
+
 void console_init(void) {
 	if(g_initialized) {
 		return;
@@ -28,6 +62,9 @@ void console_init(void) {
 	/* Create our window. */
 	g_window = newwin(CONSOLE_WIN_H, CONSOLE_WIN_W, CONSOLE_WIN_Y, CONSOLE_WIN_X);
 	assert(g_window != NULL);
+
+	/* Disable echoing. */
+	noecho();
 
 	/* Set initialized flag. */
 	g_initialized = true;
@@ -56,41 +93,54 @@ void console_printf(const char* fmt, ...) {
 }
 
 char console_read_chr(void) {
-	char out;
-	do {
-		out = wgetch(g_window);
-		if(_get_and_clear_intrpt()) {
-			return ERR;
-		}
-	} while(out == ERR);
-
-	return out;
+	char ch = (char)_get_char_impl();
+	waddch(g_window, ch);
+	return ch;
 }
 
 bool console_read_str(char* out) { return console_read_strn(out, INT_MAX); }
 
 bool console_read_strn(char* out, int max_len) {
 	/* TODO: Support backspace etc. */
-	int idx = 0;
-	char ch;
-	while(idx < max_len - 1) {
-		ch = console_read_chr();
-		if(ch == ERR) {
-			wprintw(g_window, "\n");
-			return false;
-		}
-		else if(ch == '\n') {
-			break;
-		}
+	int ch;
+	int pos = 0;
+	int cur_size = 0;
+	bool res = true;
+	bool done = false;
 
-		out[idx++] = ch;
+	/* Get current position. */
+	int start_x, start_y;
+	getyx(g_window, start_y, start_x);
+
+	while(pos < max_len - 1 && !done) {
+		ch = _get_char_impl();
+
+		/* Process char. */
+		switch(ch) {
+			case ERR: // We were interrupted, quit. 
+				res = false;
+			case '\n':
+			case '\r': // I think this is a window-ism, but eh
+				done = true;
+				break;
+			case KEY_BACKSPACE:
+			case '\b':
+			case 127:
+				/* Remove char at previous position. */
+				_shift_str_backward(out, pos - 1, max_len, start_x, start_y);
+				--pos;
+				break;
+			default:
+				/* Add char to buffer. */
+				waddch(g_window, ch);
+				out[pos++] = ch;
+				cur_size++;
+				break;
+		}
 	}
 
-	/* Move down one line. */
-	wmove(g_window, getcury(g_window) + 1, 0);
-
-	/* Clear last character. */
-	out[idx] = 0;
+	out[cur_size] = 0;
+	waddch(g_window, '\n');
 
 	return true;
 }
